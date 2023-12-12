607b72a9341c1a2b4ed3bd03329f33c9e1ba0090
asilvaolabi <asilva@olabi.org.br>
2023-12-11 21:11:13 -0300

    aula 04 - listas e keys

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -1,28 +1,62 @@
+import { useState } from "react";
 import Botao from "../Botao"
 import CampoTexto from "../CampoTexto"
+import ListaSuspensa from '../ListaSuspensa'
+import './Formulario.css';
 
 const Formulario = (props) => {
+
+  const comidas = [
+    'Hamburguer',
+    'Batata',
+    'Chocolate',
+    'Cerveja',
+    'Lasanha',
+    'Doce de Leite', 
+    'Agua de coco'
+  ]  
+
+  const [comida, setComida] = useState('');
+  const [nome, setNome] = useState('');
+  const [cargo, setCargo] = useState('');
+  const [imagem, setImagem] = useState('');
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    console.log('Foi feito o salvar', nome, cargo, imagem, comida)
+  }
+
   return(
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha com os dados mulher referência para criar um card</h2>
         <CampoTexto
             obrigatorio={true} 
             label='Nome' 
             placeholder='Digite seu nome'
+            valor={nome}
             aoAlterarCampo={valor => setNome(valor)}
         />
         <CampoTexto 
             obrigatorio={true} 
             label='Cargo' 
             placeholder='Digite seu cargo'
+            valor={cargo}
             aoAlterarCampo={valor => setCargo(valor)}
         />
         <CampoTexto 
             obrigatorio={true} 
             label='Imagem' 
             placeholder='Digite o endereço da sua imagem'
+            valor={imagem}
             aoAlterarCampo={valor => setImagem(valor)}    
+        />
+
+        <ListaSuspensa
+          label = 'Comidas'
+          itens={comidas}
+          valor={comida}
+          aoAlterarCampo={valor => setComida(valor) }
         />
         <Botao>Cadastrar</Botao>
       </form>
---
+++ src/componentes/ListaSuspensa/index.jsx
@@ -0,0 +1,18 @@
+import './ListaSuspensa.css';
+
+const ListaSuspensa = (props) => {
+    return (
+        <div className='lista-suspensa'>
+            <label>{props.label}</label>
+            <select onChange={evento => props.aoAlterarCampo(evento.target.value)} required={props.required}>
+                {
+                    props.itens.map(item => {
+                        return <option key={item}>{item}</option>
+                    })
+                }
+            </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa;
