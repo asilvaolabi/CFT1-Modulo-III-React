484c1babda33387d583548eaf1b6fe1096fc6b88
asilvaolabi <asilva@olabi.org.br>
2024-01-08 21:21:11 -0300

    Aula sobre useEffect e context API

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -1,12 +1,12 @@
-    import { useState } from "react";
-    import Botao from "../Botao"
-    import CampoTexto from "../CampoTexto"
-    import ListaSuspensa from '../ListaSuspensa'
-    import './Formulario.css';
+import React, { useState } from "react";
+import Botao from "../Botao";
+import CampoTexto from "../CampoTexto";
+import ListaSuspensa from '../ListaSuspensa';
+import './Formulario.css';
+import SelectPokemon from "../SelectPokemon";
 
-    const Formulario = ({ aoReferenciaCadastrada }) => {
-
-      const areas = [
+const Formulario = ({ aoReferenciaCadastrada }) => {
+    const areas = [
         'Gestão de Projetos',
         'UX e Design',
         'Desenvolvimento',
@@ -17,61 +17,69 @@
         'Segurança da Informação',
         'Cloud Computing',
         'Redes e Infraestrutura'
-      ]  
+    ];
 
-      const [nome, setNome] = useState('');
-      const [cargo, setCargo] = useState('');
-      const [imagem, setImagem] = useState('');
-      const [time, setTime] = useState('');
-      
+    const [nome, setNome] = useState('');
+    const [cargo, setCargo] = useState('');
+    const [imagem, setImagem] = useState('');
+    const [time, setTime] = useState('');
+    const [pokemonEscolhido, setPokemonEscolhido] = useState(''); // Estado para armazenar o Pokémon escolhido
 
-      const aoSalvar = (evento) => {
+    const aoSalvar = (evento) => {
         evento.preventDefault();
         const novoCard = {
-          nome,
-          cargo, 
-          imagem,
-          time
-        }
+            nome,
+            cargo,
+            imagem,
+            time, 
+            pokemonEscolhido // Incluindo o Pokémon escolhido nos dados do card
+        };
         aoReferenciaCadastrada(novoCard);
-      }
-
-      return(
-        <section className="formulario">
-          <form onSubmit={aoSalvar}>
-            <h3>Preencha com os dados com a sua tech referência para criar um card:</h3>
-            <CampoTexto
-                obrigatorio={true} 
-                label='Nome' 
-                placeholder='Digite seu nome'
-                valor={nome}
-                aoAlterarCampo={valor => setNome(valor)}
-            />
-            <CampoTexto 
-                obrigatorio={true} 
-                label='Cargo' 
-                placeholder='Digite seu cargo'
-                valor={cargo}
-                aoAlterarCampo={valor => setCargo(valor)}
-            />
-
-            <CampoTexto 
-                obrigatorio={true} 
-                label='Imagem' 
-                placeholder='Insira a URL da imagem'
-                valor={imagem}
-                aoAlterarCampo={valor => setImagem(valor)}
-            />
-
-            <ListaSuspensa
-              label = 'Sua referência trabalha com:'
-              itens={areas}
-              valor={time}
-              aoAlterarCampo={valor => setTime(valor) }
-            />
-            <Botao>Cadastrar</Botao>
-          </form>
-        </section> 
-      )
+        console.log(novoCard);
     }
-    export default Formulario
+
+    return (
+        <section className="formulario">
+            <form onSubmit={aoSalvar}>
+                <h3>Preencha com os dados com a sua tech referência para criar um card:</h3>
+                <CampoTexto
+                    obrigatorio={true}
+                    label='Nome'
+                    placeholder='Digite seu nome'
+                    valor={nome}
+                    aoAlterarCampo={valor => setNome(valor)}
+                />
+                <CampoTexto
+                    obrigatorio={true}
+                    label='Cargo'
+                    placeholder='Digite seu cargo'
+                    valor={cargo}
+                    aoAlterarCampo={valor => setCargo(valor)}
+                />
+
+                <CampoTexto
+                    obrigatorio={true}
+                    label='Imagem'
+                    placeholder='Insira a URL da imagem'
+                    valor={imagem}
+                    aoAlterarCampo={valor => setImagem(valor)}
+                />
+
+                <ListaSuspensa
+                    label='Sua referência trabalha com:'
+                    itens={areas}
+                    valor={time}
+                    aoAlterarCampo={valor => setTime(valor)}
+                />
+                {/* Incluindo o componente SelectPokemon no formulário */}
+                <SelectPokemon
+                    label='Escolha um Pokemon:'
+                    aoSelecionarPokemon={setPokemonEscolhido} 
+                /> 
+                <Botao>Criar Card</Botao>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario;
---
+++ src/componentes/SelectPokemon/index.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios'; // Importando Axios para fazer a requisição HTTP
+import './SelectPokemon.css';
+
+// Criar a const com a props
+const SelectPokemon = (props) => {
+    // o meu estado será iniciado com um array vazio, Estado para armazenar os Pokémons
+    const [pokemons, setPokemons] = useState([]);
+    const [selectedPokemon, setSelectedPokemon] = useState(null); // Estado para armazenar o Pokémon selecionado
+
+    //vamos usar o hook do useEfect (que executa código após o componente ser renderizado) para buscar os Pokémons da API ao montar o componente
+    useEffect(() => {
+      // Função assíncrona para buscar a lista de Pokémons da API
+        const fetchPokemons = async () => {
+          try {
+            // Requisição para a API de Pokémon
+            const response = await Axios.get('https://pokeapi.co/api/v2/pokemon');
+            setPokemons(response.data.results); // Atualiza o estado com a lista de Pokémons retornados pela API
+          } catch (error) {
+            console.error('Erro ao buscar os Pokémon:', error);
+          }
+        };
+    
+        fetchPokemons(); // Chama a função para buscar os Pokémons ao montar o componente
+      }, []); // Array vazio para garantir que o useEffect execute apenas uma vez
+
+      // Função assíncrona para buscar os detalhes de um Pokémon específico
+    const fetchPokemonDetails = async (pokemonName) => {
+      try {
+          // Requisição para a API de Pokémon usando Axios, passando o nome do Pokémon desejado
+          const response = await Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+          
+          // Define a URL da imagem do Pokémon selecionado no estado
+          setSelectedPokemon(response.data.sprites.front_default);
+          
+          // Retorna a URL da imagem do Pokémon para o componente pai usando a função passada como prop
+          props.aoSelecionarPokemon(response.data.sprites.front_default);
+      } catch (error) {
+          // Em caso de erro na requisição, exibe uma mensagem de erro no console
+          console.error('Erro ao buscar detalhes do Pokémon:', error);
+      }
+    };
+
+    // Renderiza o select com os Pokémons disponíveis
+    return (
+      <div className="select-pokemon">
+        <label>{props.label}</label>
+        <select onChange={(event) => fetchPokemonDetails(event.target.value)} required={props.required}>
+            <option value="">Selecione...</option> {/*Opção inicial para indicar ao usuário que ele deve escolher um Pokémon */}
+            {/* Itera sobre a lista de Pokémons para criar as opções no select */}
+            {pokemons.map((pokemon, index) => ( 
+                // Cria uma opção para cada Pokémon na lista
+                <option key={index} value={pokemon.name}>{pokemon.name}</option>
+            ))}
+        </select>
+      </div>
+    );
+}
+
+export default SelectPokemon;
